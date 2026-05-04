a0a14f821163560237e5ae5e2502966e380267f2
Swanand01 <swanandmathekar@gmail.com>
2026-05-04 17:38:01 +0530

    feat: add eslint and stylelint configs

---
+++ src/lint/eslint.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const wordpress = require('@wordpress/eslint-plugin');
+const commentsConfigs = require('@eslint-community/eslint-plugin-eslint-comments/configs');
+const jestPlugin = require('eslint-plugin-jest');
+
+module.exports = [
+	...wordpress.configs.recommended,
+	{
+		...commentsConfigs.recommended,
+		rules: {
+			...commentsConfigs.recommended.rules,
+		},
+	},
+	{
+		...jestPlugin.configs['flat/recommended'],
+		files: ['**/*.test.js', '**/*.test.ts'],
+	},
+];
