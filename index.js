c5f84e64e223d72619c1f29f20c796c8e641c54f
Aditya Singh <92062352+Adi-ty@users.noreply.github.com>
2026-07-30 02:43:51 +0530

    [v1.0.0] Release v1.0.0 — composer split workflow + CodeQL fixes

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const wordpress = require('@wordpress/eslint-plugin');
+const commentsConfigs = require('@eslint-community/eslint-plugin-eslint-comments/configs');
+const jestPlugin = require('eslint-plugin-jest');
+
+module.exports = [
+	...wordpress.configs.recommended,
+	commentsConfigs.recommended,
+	{
+		...jestPlugin.configs['flat/recommended'],
+		files: ['**/*.test.js'],
+	},
+];
